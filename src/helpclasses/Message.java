f88817bd485c2ecffb9a8eaf99a92f71a36364cd
Cuttlefish <Cuttlefish@dhcp-004182.wlan.ntnu.no>
2012-09-12 14:52:57 +0200

    Message-hjelpeklassen inneholder nå kontruktør.

--- src/helpclasses/Message.java
+++ src/helpclasses/Message.java
@@ -1,5 +1,22 @@
 package helpclasses;
 
 public class Message {
-
+	int messageID;
+	//datetime dateTime;
+	int pulse;
+	float temperature;
+	int positionX;
+	int positionY;
+	int sheepID;
+	int status;
+	
+	public Message (int messageID, int pulse, float temperature, int positionX, int positionY, int sheepID, int status){
+		this.messageID = messageID;
+		this.pulse = pulse;
+		this.temperature = temperature;
+		this.positionX = positionX;
+		this.positionY = positionY;
+		this.sheepID = sheepID;
+		this.status = status;
+	}
 }
