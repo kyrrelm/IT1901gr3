f5c5e1d3e61dc218dd3d770313ee928d3a1c9771
Kyrre <Kyrre@Marvin>
2012-09-18 22:00:56 +0200

    Startet på GUI. Swing skrevet/lagd i Netbeans :)

--- src/gui/GUI.java
+++ src/gui/GUI.java
@@ -1,5 +1,13 @@
 package gui;
 
-public class GUI {
+// @author Kyrre
 
+public class GUI {
+    
+     static Login login = new Login();
+     static Hub hub = new Hub();
+
+    public static void main(String[] args) {
+        login.setVisible(true);
+    }
 }
